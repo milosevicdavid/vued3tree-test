2b83c93f16317764c0ab013c04217b44f082ee9f
David <milosevicdavid@gmail.com>
2020-12-14 15:04:59 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <h1>David's vued3tree library testing</h1>
+    <div class="buttons">
+      <button @click="view = 'horizontal-graph'">Horizontal</button>
+      <button @click="view = 'circle-graph'">Circle</button>
+    </div>
+    <div class="component">
+      <transition name="component-fade" mode="out-in">
+        <component :is="view"> </component>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Horizontal from "./components/horizontal.vue";
+import Circle from "./components/circle.vue";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      view: "horizontal-graph",
+    };
+  },
+
   components: {
-    HelloWorld
-  }
-}
+    "circle-graph": Circle,
+    "horizontal-graph": Horizontal,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Oswald&display=swap");
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+#App {
+  font-family: "Oswald", sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+
+.component {
+  display: block;
+}
+
+.component-fade-enter-active,
+.component-fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+h1 {
+  background-color: black;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  color: white;
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  word-spacing: 10px;
+}
+
+button {
+  background-color: black;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  margin-right: 20px;
+  outline: none;
+  border: 1px solid black;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+    border-radius: 0;
+  }
 }
 </style>
---
+++ src/components/circle.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <h2>Circle graph</h2>
+    <div class="graph">
+      <tree
+        :data="cluster"
+        node-text="name"
+        layoutType="circular"
+        :duration="100"
+      >
+      </tree>
+    </div>
+  </div>
+</template>
+
+<script>
+import { tree } from "vued3tree";
+
+export default {
+  name: "App",
+  components: {
+    tree,
+  },
+
+  data() {
+    return {
+      cluster: {
+        name: "JavaScript Frameworks",
+        children: [
+          {
+            name: "Frontend Frameworks",
+            children: [
+              { name: "Angular" },
+              { name: "Vue.js" },
+              { name: "React" },
+              { name: "Svelte" },
+            ],
+          },
+          {
+            name: "Backend Frameworks",
+            children: [
+              { name: "Express" },
+              { name: "Koa" },
+              { name: "Meteor" },
+              { name: "Sails" },
+            ],
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style>
+h2 {
+  text-align: center;
+}
+
+.graph {
+  width: 700px;
+  height: 700px;
+}
+</style>
